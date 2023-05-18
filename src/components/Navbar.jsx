2d9ea53e5c3229ce72ac40938fad1f1d6ac6e889
Sajson <amokrzycki96@gmail.com>
2023-05-18 12:33:52 +0200

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+const Navbar = () => {
+  return (
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0A192F] text-gray-400"></div>
+  );
+};
+
+export default Navbar;
